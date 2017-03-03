248c364b76c9bd3ad1f1632dc6399ec651870c9d
raymoo <uguu@installgentoo.com>
2017-03-02 17:30:04 -0800

    Make FieldTrack a control so 2D things can be added, also ignore non field elements

--- Song/FieldTrack.gd
+++ Song/FieldTrack.gd
@@ -1,4 +1,4 @@
-extends Spatial
+extends Control
 
 var time = 0
 
@@ -7,7 +7,7 @@ func _ready():
 
 func _process(delta):
 	time += delta
-	var elements = get_children()
+	var elements = get_tree().get_nodes_in_group("FieldElement")
 	for element in elements:
 		element.update(time)
 
@@ -15,7 +15,10 @@ func is_affected(button_index, element):
 	return button_index >= element.start_lane() and button_index <= element.end_lane()
 
 func handle_press(button_index):
-	var elements = get_children()
+	var elements = get_tree().get_nodes_in_group("FieldElement")
 	for element in elements:
 		if is_affected(button_index, element):
 			element.press(time)
+
+func handle_move(from, to):
+	handle_press(to)
