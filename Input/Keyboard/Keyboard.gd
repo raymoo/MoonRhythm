8cd18eb988e70555b5ec07b8f32a696ac663d419
raymoo <uguu@installgentoo.com>
2017-10-19 11:58:17 -0700

    Fix test scene

--- Input/Keyboard/Keyboard.gd
+++ Input/Keyboard/Keyboard.gd
@@ -6,8 +6,8 @@ onready var button_count = buttons.size()
 var touches_by_button = {}
 var buttons_by_touch = {}
 
-const button_up = preload("res://Keyboard/button.png")
-const button_down = preload("res://Keyboard/button_down.png")
+const button_up = preload("res://Input/Keyboard/button.png")
+const button_down = preload("res://Input/Keyboard/button_down.png")
 
 # Finger was pressed down
 signal pressed(button_index)
